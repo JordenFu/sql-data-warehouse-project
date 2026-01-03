16fdfd7abafd3914c2493fa2ed08f44034730a28
Jorden <158175247+JordenFu@users.noreply.github.com>
2026-01-04 05:32:44 +0800

    Implement cumulative sales analysis query
    Add a query to calculate monthly total sales and running totals.

--- scripts/data-analytics/cumulative_analysis.sql
+++ scripts/data-analytics/cumulative_analysis.sql
@@ -11,3 +11,22 @@ SQL Functions Used:
     - Window Functions: SUM() OVER(), AVG() OVER()
 ===============================================================================
 */
+
+
+-- Calculate the total sales per month
+-- and the running total of sales over time
+-- (reset each year)
+SELECT
+	FORMAT(order_date, 'yyyy-MM') AS order_date,
+	total_sales,
+	SUM(total_sales) OVER (PARTITION BY YEAR(order_date) ORDER BY order_date) AS running_total_sales,
+	AVG(avg_price) OVER (ORDER BY order_date) AS moving_average_price
+FROM (
+	SELECT
+		DATETRUNC(month, order_date) AS order_date,
+		SUM(sales_amount) AS total_sales,
+		AVG(price) AS avg_price
+	FROM gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(month, order_date)
+)t
